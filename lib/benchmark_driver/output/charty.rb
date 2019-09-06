3cdafc6b5510692367121fdcc1d686ab462cfc5e
Takashi Kokubun <takashikkbn@gmail.com>
2019-09-07 00:13:03 +0900

    Support box_plot for single job

--- lib/benchmark_driver/output/charty.rb
+++ lib/benchmark_driver/output/charty.rb
@@ -5,6 +5,7 @@ class BenchmarkDriver::Output::Charty < BenchmarkDriver::BulkOutput
   GRAPH_PATH = 'charty.png'
 
   OPTIONS = {
+    chart: ['--output-chart CHART', Regexp.union(['bar', 'box']), 'Specify chart type: bar, box (default: bar)'],
     path: ['--output-path PATH', String, "Chart output path (default: #{GRAPH_PATH})"]
   }
 
@@ -14,6 +15,7 @@ class BenchmarkDriver::Output::Charty < BenchmarkDriver::BulkOutput
   def initialize(contexts:, options:, **)
     super
     @contexts = contexts
+    @chart = options.fetch(:chart, 'bar')
     @path = options.fetch(:path, GRAPH_PATH)
   end
 
@@ -28,29 +30,47 @@ class BenchmarkDriver::Output::Charty < BenchmarkDriver::BulkOutput
       job = job_context_result.keys.first
 
       names = job_context_result[job].keys.map(&:name)
-      values = job_context_result[job].values.map { |result| result.values.fetch(metric) }
-      barh = charty.barh do
-        series names, values
-        ylabel metric.unit
+      case @chart
+      when 'bar'
+        values = job_context_result[job].values.map { |result| result.values.fetch(metric) }
+        chart = charty.barh do
+          series names, values
+          ylabel metric.unit
+        end
+      when 'box'
+        values = job_context_result[job].values.map { |result| result.all_values.fetch(metric) }
+        chart = charty.box_plot do
+          labels names
+          data values
+          ylabel metric.unit
+        end
+      else
+        raise ArgumentError, "unexpected --output-chart: #{@chart}"
       end
-      barh.save(@path)
     else
       jobs = job_context_result.keys
-      values = @contexts.map{|context|
-        [
-          jobs.map{|job| "#{job.name}(#{context.name})" },
-          jobs.map{|job| job_context_result[job][context].values.fetch(metric).round }
-        ]
-      }
 
-      barh = charty.barh do
-        values.each do |value|
-          series *value
+      case @chart
+      when 'bar'
+        values = @contexts.map{|context|
+          [
+            jobs.map{|job| "#{job.name}(#{context.name})" },
+            jobs.map{|job| job_context_result[job][context].values.fetch(metric).round }
+          ]
+        }
+        chart = charty.barh do
+          values.each do |value|
+            series *value
+          end
+          ylabel metric.unit
         end
-        ylabel metric.unit
+      when 'box'
+        raise NotImplementedError, "--output-chart=box is not supported with multiple jobs"
+      else
+        raise ArgumentError, "unexpected --output-chart: #{@chart}"
       end
-      barh.save(@path)
     end
+    chart.save(@path)
     puts ": #{@path}"
   end
 
