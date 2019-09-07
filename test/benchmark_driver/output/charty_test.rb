fe0bc813703d3b1d07ad702fb653187d8b6bb83b
284km <k.furuhashi10@gmail.com>
2019-09-07 11:18:26 +0700

    Remove unused test

--- test/benchmark_driver/output/charty_test.rb
+++ test/benchmark_driver/output/charty_test.rb
@@ -1,10 +1,6 @@
 require "test_helper"
 
 class BenchmarkDriver::Output::ChartyTest < Test::Unit::TestCase
-  def test_that_it_has_a_version_number
-    refute_nil ::BenchmarkDriver::Output::Charty::VERSION
-  end
-
   def test_it_does_something_useful
     assert false
   end
